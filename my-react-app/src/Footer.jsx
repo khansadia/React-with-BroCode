ee36fbc95474cc82fe553b4a97957fd944851971
Sadia Khan <khansadia220@gmail.com>
2024-03-07 11:22:56 +0100

    So far nav, footer, card is added

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer(){
+    return(
+<Footer>
+    <p> &copy;{new Date().getFullYear()} Your website Name</p>
+</Footer>
+    );
+}
+export default Footer
---
+++ my-react-app/src/assets/Card.jsx
@@ -0,0 +1,14 @@
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src={profilePic} alt="profile picture"></img>
+            <h2 className="card-title">Khan</h2>
+            <p className="card-text">I make react apps and playing with css</p>
+
+        </div>
+
+
+    );
+
+}
+export default Card
